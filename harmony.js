000638d7f53ea5678c1fc7f2f0cd3151e69fae0e
Kirai Chan <xkiraichan@gmail.com>
2022-02-11 17:34:38 -0500

    Declare global over returning function

--- harmony.js
+++ harmony.js
@@ -1,16 +1,14 @@
-(() => {
-    return {
-        __getFuncPatch: (hook, orig) => function (...args) {
-            let res = hook.apply(this, [...args, orig]);
-            if (res === false) return;
-            else if (Array.isArray(res)) args = res;
-            orig.apply(this, [...args]);
-        },
-        patchFunction: function(type, name, hook) {
-            if (typeof type == "object") {
-                if (!type.__proto__[name]) type[name] = this.__getFuncPatch(type[name], hook);
-                else type.__proto__[name] = this.__getFuncPatch(type.__proto__[name], hook);
-            } else type.prototype[name] = this.__getFuncPatch(type.prototype[name], hook);
-        }
-    };
-})
+harmony = {
+    __getFuncPatch: (hook, orig) => function (...args) {
+        let res = hook.apply(this, [...args, orig]);
+        if (res === false) return;
+        else if (Array.isArray(res)) args = res;
+        orig.apply(this, [...args]);
+    },
+    patchFunction: function (type, name, hook) {
+        if (typeof type == "object") {
+            if (!type.__proto__[name]) type[name] = this.__getFuncPatch(type[name], hook);
+            else type.__proto__[name] = this.__getFuncPatch(type.__proto__[name], hook);
+        } else type.prototype[name] = this.__getFuncPatch(type.prototype[name], hook);
+    }
+};
